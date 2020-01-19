af15218651434a1cd954103938aa6871d90a5f03
lewin <lewin2519@gmail.com>
2020-01-19 10:43:01 +0800

    添加注释

--- Dynamic Programming/c0001.c
+++ Dynamic Programming/c0001.c
@@ -1,56 +1,37 @@
 #include<stdio.h>
-#include<math.h>
-#include<stdlib.h>
 
 #define MAXSIZE 100
+#define MAX(a, b) (a>b)?a:b
+
 int dp[MAXSIZE][MAXSIZE]; 
 int tower[MAXSIZE][MAXSIZE];
 
-// void number_tower(int *tower, int n){
-//     int i;
-//     int j;
+// int max(int a, int b){
 
-//     for(i = n; i <= 1; i--){
-//         if(i == n)
-//             for(j = 1; j <= i; j++)
-//                 dp[i][j] = tower[i][j];
-//         else
-//             for(j = 1; j <= i; j++)
-//                 dp[i][j] = max(dp[i+1][j], dp[i+1][j+1]) + tower[i][j];
-//     }
-
+//     return a>b ? a : b;
 // }
-int max(int a, int b){
-
-    return a>b ? a : b;
-}
 
 int main(void){
     int n;
     int i, j;
-
+    //输入数塔的层数
     printf("please, enter the n:");
     scanf("%d", &n);
+    //输入数塔的内容
     for(i=1; i<=n; i++){
         for(j=1; j<=i; j++)
             scanf("%d", &tower[i][j]);
     }
-    // for(i=1; i<=n; i++){
-    //     for(j=1; j<=i; j++)
-    //         printf("it %d\n", tower[i][j]);
-    // }
-
-    // printf("it is a %d\n", tower[1][1]);
-
+    //采用递推方式求解数塔的全局最优
     for(i = n; i >= 1; i--){
         if(i == n)
             for(j = 1; j <= i; j++){
                 dp[i][j] = tower[i][j];
-                // printf(" motho %d", dp[i][j]);
                 }
         else
             for(j = 1; j <= i; j++)
-                dp[i][j] = max(dp[i+1][j], dp[i+1][j+1]) + tower[i][j];
+                //转移方程
+                dp[i][j] = MAX(dp[i+1][j], dp[i+1][j+1]) + tower[i][j];
     }
 
     printf("The result is %d", dp[1][1]);
